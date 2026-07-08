0cdbe5adff990ff17ed08b2ba6a31658717f313b
Adarsh <adarshm.off@gmail.com>
2026-07-08 12:33:11 +0530

    updated hero content position|

--- myportfolio/urls.py
+++ myportfolio/urls.py
@@ -21,7 +21,7 @@ from django.conf.urls.static import static
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('adarsh-portfolio-config/', admin.site.urls),
     path('', include('core.urls')),
 ]
 
