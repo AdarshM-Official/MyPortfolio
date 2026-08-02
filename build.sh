e13d199bcdd3ed606d46f48f5fca68a7055d0974
Adarsh <adarshm.off@gmail.com>
2026-08-02 12:21:14 +0530

    vercel update 2

--- build.sh
+++ build.sh
@@ -2,5 +2,8 @@
 # exit on error
 set -o errexit
 
+python3 -m venv venv
+source venv/bin/activate
+pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
