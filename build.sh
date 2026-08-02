45eb272b2ce2cac02aeece796e11ff37aa50ea5a
Adarsh <adarshm.off@gmail.com>
2026-08-02 14:57:40 +0530

    updated vercel.json

--- build.sh
+++ build.sh
@@ -1,19 +1,8 @@
-{
-  "version": 2,
-  "builds": [
-    {
-      "src": "myportfolio/wsgi.py",
-      "use": "@vercel/python"
-    }
-  ],
-  "routes": [
-    {
-      "src": "/static/(.*)",
-      "dest": "/static/$1"
-    },
-    {
-      "src": "/(.*)",
-      "dest": "myportfolio/wsgi.py"
-    }
-  ]
-}
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
+
+python3 -m venv venv
+venv/bin/pip install -r requirements.txt
+venv/bin/python manage.py collectstatic --no-input
+venv/bin/python manage.py migrate
