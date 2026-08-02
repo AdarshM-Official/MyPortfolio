ba83060b857cae1f75522b81b20bae2fd7dd96d8
Adarsh <adarshm.off@gmail.com>
2026-08-02 12:18:22 +0530

    vercel update 2

--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -87,9 +87,10 @@ WSGI_APPLICATION = 'myportfolio.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default='sqlite:///' + str(BASE_DIR / 'db.sqlite3'),
-        conn_max_age=600
+    "default": dj_database_url.config(
+        default=os.environ.get("DATABASE_URL"),
+        conn_max_age=600,
+        ssl_require=True,
     )
 }
 
